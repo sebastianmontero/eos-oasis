e53eb1ca5ed02b8ce321ffbccde2eb577c3e52a0
Sebastian Montero <smontero@mightywizards.com>
2018-09-25 22:56:20 +0900

    Moved most of the code to the Players header file, except the action implementations

--- contracts/players/players.cpp
+++ contracts/players/players.cpp
@@ -1,94 +1,66 @@
 #include "players.hpp"
 
 namespace Oasis {
-    using namespace eosio;
-    using std::string;
+    
+    //@abi action
+    void Players::add(const account_name account, string& username ) {
+        require_auth(account);
+
+        playerIndex players(_self, _self);
+
+        auto iterator = players.find(account);
+
+        eosio_assert(iterator == players.end(), "Adress for account already exists");
+
+        players.emplace(account, [&](auto& player){
+            player.account_name = account;
+            player.username = username;
+            player.level = 1;
+            player.health_points = 1000;
+            player.energy_points = 1000;
+        });
 
 
-    class Players : public contract {
-        using contract::contract;
+    }
 
-        public:
-            Players(account_name self): contract(self){}
+    //@abi action
+    void Players::update(const account_name account, uint64_t level, int64_t healthPoints, int64_t energyPoints){
+        require_auth(account);
 
-            //@abi action
-            void add(const account_name account, string& username ) {
-                require_auth(account);
+        playerIndex players(_self, _self);
 
-                playerIndex players(_self, _self);
+        auto iterator = players.find(account);
 
-                auto iterator = players.find(account);
+        eosio_assert(iterator != players.end(), "Adress for account does not exist");
 
-                eosio_assert(iterator == players.end(), "Adress for account already exists");
+        players.modify(iterator, account, [&](auto& player){
+            player.level = 1;
 
-                players.emplace(account, [&](auto& player){
-                    player.account_name = account;
-                    player.username = username;
-                    player.level = 1;
-                    player.health_points = 1000;
-                    player.energy_points = 1000;
-                });
-
-
+            if((player.health_points - healthPoints) < 0){
+                player.health_points = 0;
+            } else {
+                player.health_points -=  healthPoints;
             }
 
-            //@abi action
-            void update(const account_name account, uint64_t level, int64_t healthPoints, int64_t energyPoints){
-                require_auth(account);
-
-                playerIndex players(_self, _self);
-
-                auto iterator = players.find(account);
-
-                eosio_assert(iterator != players.end(), "Adress for account does not exist");
-
-                players.modify(iterator, account, [&](auto& player){
-                    player.level = 1;
-
-                    if((player.health_points - healthPoints) < 0){
-                        player.health_points = 0;
-                    } else {
-                        player.health_points -=  healthPoints;
-                    }
-
-                    if((player.energy_points - energyPoints) < 0){
-                        player.energy_points = 0;
-                    } else {
-                        player.energy_points -=  energyPoints;
-                    }
-
-                });
-
+            if((player.energy_points - energyPoints) < 0){
+                player.energy_points = 0;
+            } else {
+                player.energy_points -=  energyPoints;
             }
 
-            //@abi action
-            void getplayer(const account_name account){
-                playerIndex players(_self, _self);
+        });
 
-                auto iterator = players.find(account);
-                eosio_assert(iterator != players.end(), "Account not found");
+    }
 
-                auto currentPlayer = players.get(account);
-                print("Username: ", currentPlayer.username.c_str(), " Level: ", currentPlayer.level, " Health: ", currentPlayer.health_points, " Energy: ", currentPlayer.energy_points);
-            }
+    //@abi action
+    void Players::getplayer(const account_name account){
+        playerIndex players(_self, _self);
 
-        private:
+        auto iterator = players.find(account);
+        eosio_assert(iterator != players.end(), "Account not found");
 
-            //@abi table player i64
-            struct player {
-                uint64_t account_name;
-                string username;
-                uint64_t level;
-                uint64_t health_points = 1000;
-                uint64_t energy_points = 1000;
+        auto currentPlayer = players.get(account);
+        print("Username: ", currentPlayer.username.c_str(), " Level: ", currentPlayer.level, " Health: ", currentPlayer.health_points, " Energy: ", currentPlayer.energy_points);
+    }
 
-                uint64_t primary_key() const {return account_name;}
-
-                EOSLIB_SERIALIZE(player, (account_name)(username)(level)(health_points)(energy_points))
-            };
-
-            typedef multi_index<N(player), player> playerIndex;      
-    };
-
-    EOSIO_ABI(Players, (add)(update)(getplayer))
 }
